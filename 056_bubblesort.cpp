adf8afc38d6c6f2fd530f020844f35a62840aac0
Dababah <pawasalee96@gmail.com>
2024-03-08 17:39:11 +0700

    step 4

--- 056_bubblesort.cpp
+++ 056_bubblesort.cpp
@@ -32,10 +32,18 @@ void input() {
 void  bubblesortArray () { // prosedur mengurutkan array dengan metode bubble sort
 	int pass = 1; // step 1
 
-	do { 
-		for (int j = 0; j <= n - 1 pass; j++) { // step 2
-	
-	}
+	do {
+		for (int j = 0; j <= n - 1 pass; j++) {// step 2
+			if (arr[j] > arr[j + 1]) { // step 3
+				int temp;
+				arr[j] = arr[k + 1];
+				arr[j + 1] = temp;
+
+			}
+
+	    }
+		pass = pass + 1; // step 4
+		
 
 
 
