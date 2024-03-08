6ecf4d4fe26b7e6a725a4cfc4fe51652ecb4512f
Dababah <pawasalee96@gmail.com>
2024-03-08 17:29:25 +0700

    prosedur mengurutkan array

--- 056_bubblesort.cpp
+++ 056_bubblesort.cpp
@@ -8,7 +8,29 @@ using namespace std;
 int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
 int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen array 
 
-void input() 
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : "; // output kelayar
+		cin >> n; // input dari pengguna 
+		if (n <= 20) // jika n kurang dari atau sama dengan 20 
+			break;  // keluar loop
+		else {  // jika n lebih dari 20
+			cout << "\Array dapat mempunyai maksimal 20 elemen. \n"; // output ke layar
+
+	}
+}
+
+	cout << endl;
+	cout <<  "======================" << endl;
+	cout <<  " Masukkan Elemen Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i ++)  { // looping dengan i di mulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": "; // output ke layar
+		cin >> arr[i];                         // ouput dari pengguna
+
+void  bubblesortArray () 
+
 
 
 
